9f7e26a93c3f7c5be677509f2a5d32274ae27928
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-11-24 09:08:57 +0600

    Update HomeFragment.kt

--- app/src/main/java/com/livetvpro/ui/home/HomeFragment.kt
+++ app/src/main/java/com/livetvpro/ui/home/HomeFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.SearchView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.livetvpro.MainActivity
 import com.livetvpro.R
 import com.livetvpro.databinding.FragmentHomeBinding
 import com.livetvpro.ui.adapters.CategoryAdapter
@@ -25,8 +22,6 @@ class HomeFragment : Fragment() {
 
     private val viewModel: HomeViewModel by viewModels()
     private lateinit var categoryAdapter: CategoryAdapter
-    
-    private var searchView: SearchView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,52 +34,9 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupToolbar()
         setupRecyclerView()
         observeViewModel()
         setupSwipeRefresh()
-    }
-
-    private fun setupToolbar() {
-        // Setup search button
-        (activity as? MainActivity)?.findViewById<View>(R.id.btn_search)?.setOnClickListener {
-            showSearchDialog()
-        }
-
-        // Setup favorites button
-        (activity as? MainActivity)?.findViewById<View>(R.id.btn_favorites)?.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_favorites)
-        }
-
-        // Setup theme toggle button
-        (activity as? MainActivity)?.findViewById<View>(R.id.btn_theme_toggle)?.setOnClickListener {
-            (activity as? MainActivity)?.toggleTheme()
-        }
-    }
-
-    private fun showSearchDialog() {
-        val searchView = SearchView(requireContext())
-        searchView.queryHint = "Search categories..."
-        
-        val dialog = MaterialAlertDialogBuilder(requireContext())
-            .setTitle("Search Categories")
-            .setView(searchView)
-            .setNegativeButton("Close", null)
-            .create()
-
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                viewModel.searchCategories(query ?: "")
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                viewModel.searchCategories(newText ?: "")
-                return true
-            }
-        })
-
-        dialog.show()
     }
 
     private fun setupRecyclerView() {
